63f5aaf763c6afe4dd7dd505b9d4ac203592d8bd
= <yaakovtayeb@gmail.com>
2017-09-18 09:15:09 +0300

    add countries to the monitoring

--- Python/Accuracy_Plots_Learningsets.py
+++ Python/Accuracy_Plots_Learningsets.py
@@ -43,23 +43,24 @@ err_margin = 0.3
 
 # Filter data
 # data = data[data["visitsonline"]>800000]
-# data = data[data["category"]=="Internet_and_Telecom"]
 
-for country in [458]:
+for country in [152]:
     for date in set(data["date"]):
-        # for device in devices:
-        err = data.loc[(data['country'] == country) & (data['date'] == date) & (data['ga_mobile'] > sitesizeforerror)][['ga_mobile', 'sw_mobile']].values
-        x = data.loc[(data['country'] == country) & (data['date'] == date)]["ga_mobile"] # GA data for a specific month
-        y = data.loc[(data['country'] == country) & (data['date'] == date)]["sw_mobile"] # SW
-        sites_text = data.loc[(data['country'] == country) & (data['date'] == date)]["site"].values
-        categories = data.loc[(data['country'] == country) & (data['date'] == date)]["category"].values
-        setcolors = list(set(categories))
-        colors = map(lambda x: categoryColor(x, setcolors), categories)
+        tmpdata = data.loc[(data['country'] == country) & (data['date'] == date)]
+        # filter the data
+        tmpdata = tmpdata.loc[(data['category'] == "Internet_and_Telecom") & (data["ga_mobile"]>100000)]
+        err = tmpdata[(data['ga_mobile'] > sitesizeforerror)][['ga_mobile', 'sw_mobile']].values
+        x = tmpdata["ga_mobile"] # GA data for a specific month
+        y = tmpdata["sw_mobile"] # SW
+        sites_text = tmpdata["site"].values
+        categories = tmpdata["category"].values
+        colors = categoryColor(categories)
         dots = (np.array(y)/100000 + 1).astype(int)
         f = np.poly1d(np.polyfit(x, y, 1))
+
         trace1 = Scatter(
-            x=x,
-            y=y,
+            x=tmpdata["ga_mobile"],
+            y=tmpdata["sw_mobile"],
             mode='markers',
             name='Visits',
             marker = dict(
@@ -101,7 +102,7 @@ for country in [458]:
         country_list.append(country)
         date_list.append(date)
 
-        plotTitle = "US Mobile Accuracy Check. Month: %s. R: %2f, d: %.2f" % (date, R_list[-1], delta_list[-1])
+        plotTitle = "%d Mobile Accuracy Check. Month: %s. R: %2f, d: %.2f" % (country, date, R_list[-1], delta_list[-1])
         layout = Layout(
             title=plotTitle,
             xaxis=dict(
@@ -113,7 +114,7 @@ for country in [458]:
         )
         fig = Figure(data=plotdata, layout=layout)
         plot(fig)
-        time.sleep(1)
+        time.sleep(2)
         # raw_input("Press Enter to continue...")  # stop for manual processing
 
 Results = pd.DataFrame({'Country': country_list, 'Date': date_list, 'R': R_list, 'delta': delta_list, 'AbsDelata': AbsDelta_list, 'Accuracy_0.3': abs_error})
